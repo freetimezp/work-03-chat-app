83276976bdbd00c5933a499b4b682492a0646ed2
freetimezp <kraftukrnet@ukr.net>
2024-09-26 22:15:07 +0300

    04. work-03-chat-app. Signup form.

---
+++ signup.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Project | Chat App | Signup</title>
+</head>
+
+<body>
+    <div id="form-wrapper">
+        <div class="form-header">
+            <span>My Chat</span>
+            <div>Login</div>
+        </div>
+
+        <form>
+            <input type="text" name="username" placeholder="Username"><br>
+
+            <div class="gender">
+                <br>Gender:<br>
+                <input type="radio" name="gender"> Male <br>
+                <input type="radio" name="gender"> Female <br>
+            </div>
+
+            <input type="password" name="password" placeholder="Password"><br>
+            <input type="password" name="password2" placeholder="Password Confirm"><br>
+            <input type="submit" value="Sign Up"><br>
+        </form>
+    </div>
+</body>
+
+</html>
