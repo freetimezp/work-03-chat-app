5b48225f141c6816ea3e2b2ef2011ccd680e0738
freetimezp <kraftukrnet@ukr.net>
2024-09-26 20:54:14 +0300

    03. work-03-chat-app. AJAX basic.

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+function __(element) {
+    return document.getElementById(element);
+}
+
+var label = __("label_chat");
+
+label.addEventListener("click", function () {
+    var inner_pannel = __("inner_left_pannel");
+
+    var ajax = new XMLHttpRequest();
+    ajax.onload = function () {
+        if (ajax.status == 200 || ajax.readyState == 4) {
+            inner_pannel.innerHTML = ajax.responseText;
+        }
+    };
+    ajax.open("POST", "file.php", true);
+    ajax.send();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ file.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "some data from file";
